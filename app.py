6a80691a6ba9926d75069d910328469ad8affa4a
Jonathan Hsu <jonshsu@gmail.com>
2019-10-06 00:46:01 -0700

    fix db

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 import sys
 from flask_heroku import Heroku
 app = Flask( __name__ )
-app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 heroku = Heroku(app)
 db = SQLAlchemy(app)
