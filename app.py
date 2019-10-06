b313e265c0234344d7bdfbdfcaf5c76f8869b6a1
Jonathan Hsu <jonshsu@gmail.com>
2019-10-05 23:55:32 -0700

    migrations

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ db = SQLAlchemy(app)
 
 class SpreadsheetData(db.Model):
     __tablename__ = "spreadsheetdata"
+    
     id = db.Column(db.Integer, primary_key=True)
     mydata = db.Column(db.Text())
 
